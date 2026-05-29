fb582c2675b3dbdb2b38f87c3edb33ace05b471c
Vong Ly <vongly62@gmail.com>
2026-05-29 00:16:33 -0700

    initial commit

--- alembic/env.py
+++ alembic/env.py
@@ -1,7 +1,8 @@
+import os
 from logging.config import fileConfig
 
 from sqlalchemy import engine_from_config
-from sqlalchemy import pool
+from sqlalchemy import pool, create_engine
 
 from alembic import context
 
@@ -26,28 +27,26 @@ target_metadata = None
 # ... etc.
 
 
-def run_migrations_offline() -> None:
-    """Run migrations in 'offline' mode.
+def run_migrations_online() -> None:
+    database_url = os.getenv("DATABASE_URL")
 
-    This configures the context with just a URL
-    and not an Engine, though an Engine is acceptable
-    here as well.  By skipping the Engine creation
-    we don't even need a DBAPI to be available.
+    if not database_url:
+        raise Exception("DATABASE_URL is not set. Check your .env file.")
 
-    Calls to context.execute() here emit the given string to the
-    script output.
-
-    """
-    url = config.get_main_option("sqlalchemy.url")
-    context.configure(
-        url=url,
-        target_metadata=target_metadata,
-        literal_binds=True,
-        dialect_opts={"paramstyle": "named"},
+    connectable = create_engine(
+        database_url,
+        poolclass=pool.NullPool,
     )
 
-    with context.begin_transaction():
-        context.run_migrations()
+    with connectable.connect() as connection:
+        context.configure(
+            connection=connection,
+            target_metadata=target_metadata,
+        )
+
+        with context.begin_transaction():
+            context.run_migrations()
+
 
 
 def run_migrations_online() -> None:
