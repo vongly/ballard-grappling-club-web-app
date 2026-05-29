8dd23933a51d036c3afde575406a572b3b56ce0f
Vong Ly <vongly62@gmail.com>
2026-05-29 01:13:23 -0700

    initial commit

--- alembic/env.py
+++ alembic/env.py
@@ -1,5 +1,3 @@
-import os
-
 from logging.config import fileConfig
 
 from sqlalchemy import engine_from_config
@@ -10,11 +8,6 @@ from alembic import context
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
 config = context.config
-
-config.set_main_option(
-    "sqlalchemy.url",
-    os.environ["DATABASE_URL"]
-)
 
 # Interpret the config file for Python logging.
 # This line sets up loggers basically.
