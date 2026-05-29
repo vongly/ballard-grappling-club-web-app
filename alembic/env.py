ce87846f920c1fd8e7bd665b8911a423a9ce1aea
Vong Ly <vongly62@gmail.com>
2026-05-29 00:33:01 -0700

    initial commit

--- alembic/env.py
+++ alembic/env.py
@@ -1,14 +1,20 @@
 import os
+
 from logging.config import fileConfig
 
 from sqlalchemy import engine_from_config
-from sqlalchemy import pool, create_engine
+from sqlalchemy import pool
 
 from alembic import context
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
 config = context.config
+
+config.set_main_option(
+    "sqlalchemy.url",
+    os.environ["DATABASE_URL"]
+)
 
 # Interpret the config file for Python logging.
 # This line sets up loggers basically.
@@ -27,26 +33,28 @@ target_metadata = None
 # ... etc.
 
 
-def run_migrations_online() -> None:
-    database_url = os.getenv("DATABASE_URL")
+def run_migrations_offline() -> None:
+    """Run migrations in 'offline' mode.
 
-    if not database_url:
-        raise Exception("DATABASE_URL is not set. Check your .env file.")
+    This configures the context with just a URL
+    and not an Engine, though an Engine is acceptable
+    here as well.  By skipping the Engine creation
+    we don't even need a DBAPI to be available.
 
-    connectable = create_engine(
-        database_url,
-        poolclass=pool.NullPool,
+    Calls to context.execute() here emit the given string to the
+    script output.
+
+    """
+    url = config.get_main_option("sqlalchemy.url")
+    context.configure(
+        url=url,
+        target_metadata=target_metadata,
+        literal_binds=True,
+        dialect_opts={"paramstyle": "named"},
     )
 
-    with connectable.connect() as connection:
-        context.configure(
-            connection=connection,
-            target_metadata=target_metadata,
-        )
-
-        with context.begin_transaction():
-            context.run_migrations()
-
+    with context.begin_transaction():
+        context.run_migrations()
 
 
 def run_migrations_online() -> None:
