79aa5279c0d42cf654d2837010341315a5bd4ca3
Vong Ly <vongly62@gmail.com>
2026-06-17 14:11:57 -0700

    initial commit

--- frontend/routes/api/class_.py
+++ frontend/routes/api/class_.py
@@ -5,7 +5,7 @@ from flask import Blueprint, render_template, request, session, redirect, flash,
 
 from urllib.parse import urlencode
 import requests
-from datetime import datetime, timedelta
+from datetime import datetime, timedelta, date
 
 sys.path.append(str(Path(__file__).resolve().parents[2]))
 
@@ -243,6 +243,7 @@ def class_list():
 
     return render_template(
         "classes/class_list.html",
+        today=date.today(),
         next_week=sort_desc(next_week),
         current_week=sort_desc(current_week),
         current_month=sort_desc(current_month),
